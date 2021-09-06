f3d969d88d6637f1543fe0fbe71fb7a2eb7d126e
samtroutman <troutm16@gmail.com>
2021-09-05 20:43:22 -0400

    commit tree b155ac4dba7dea9e68132340aae0a63ec52129cf

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+function map(src, callback) {
+    const r= [];
+
+    for (let i=0; i < src.length; i++) {
+        const value = src[i];
+        r.push(callback(value));
+    }
+    return r;
+}
+
+function reduce(src, callback, startingPoint) {
+    let total;
+    if (startingPoint) {
+        total = startingPoint;
+        for (let i = 0; i < src.length; i++) {
+            total = callback(src[i], total);
+          }
+    return total;
+    }
+    else {
+        total = src[0];
+        for (let i = 1; i < src.length; i++) {
+          total = callback(src[i], total);
+        }
+    return total
+
+    }
+  }
